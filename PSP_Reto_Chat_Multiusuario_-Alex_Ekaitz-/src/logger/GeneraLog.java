4deb0e3b14b3dfce4a1b50d2a4b5038471346f1c
Alex <AlexIrazola@gmail.com>
2025-10-16 13:25:40 +0200

    Modificado y comentado el logger.

--- PSP_Reto_Chat_Multiusuario_-Alex_Ekaitz-/src/logger/GeneraLog.java
+++ PSP_Reto_Chat_Multiusuario_-Alex_Ekaitz-/src/logger/GeneraLog.java
@@ -7,20 +7,28 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class GeneraLog {
+public class GeneraLog 
+{
+	// [ VARIABLES ]
     private static Logger logger = null;
     private static FileHandler fileHandler = null;
 
+    // [ CONSTRUCTORES ]
     // Constructor privado para evitar la instanciación externa
-    private GeneraLog() {
+    private GeneraLog() 
+    {
         // Inicializa el logger solo si no se ha inicializado previamente
-        if (logger == null) {
+        if (logger == null) 
+        {
         	File carpeta = new File("./logs"); // Ruta de la carpeta
 
-            if (!carpeta.exists())
-                carpeta.mkdir();
-
-            try {
+            if (!carpeta.exists()) 
+            {
+            	carpeta.mkdir();
+            }
+                
+            try 
+            {
                 // Configura el logger
                 logger = Logger.getLogger(GeneraLog.class.getName());
                 // A false evita la salida en consola
@@ -31,24 +39,31 @@ public class GeneraLog {
                 fileHandler.setFormatter(formatter);
                 logger.addHandler(fileHandler);
                 logger.setLevel(Level.ALL); // Establece el nivel de log (puedes ajustarlo según tu necesidad)
-            } catch (IOException e) {
-                System.out.println("Error al configurar el logger: " + e.getMessage());
+            } 
+            catch (IOException e) 
+            {
+                System.err.println("[ERROR AL CONFIGURAR EL LOGGER]: " + e.getMessage());
             }
         }
     }
 
+    // [ METODOS ]
     // Método para obtener la instancia del logger
-    public static Logger getLogger() {
+    public static Logger getLogger() 
+    {
         if (logger == null)
+        {
             new GeneraLog(); // Si aún no se ha creado, inicialízalo
-
+        }
         return logger;
     }
 
     // Método para cerrar el fileHandler
-    public static void closeLogger() {
+    public static void closeLogger() 
+    {
         if (fileHandler != null)
+        {
             fileHandler.close();
-
+        }
     }
 }
